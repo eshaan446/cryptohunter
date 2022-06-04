bdbe856e98348acad9fb3c18dd3df824f6c614c1
Eshaan <eshaanpandeyk540@gmail.com>
2022-06-05 00:56:12 +0530

    Updates

---
+++ precache-manifest.e430e28033040a5fef38f694862df11d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9bf813966519e9a5ac1bbff639ab436",
+    "url": "/cryptohunter/index.html"
+  },
+  {
+    "revision": "7497d8db9190c3fd3221",
+    "url": "/cryptohunter/static/css/main.2eaeef33.chunk.css"
+  },
+  {
+    "revision": "77148196acdc096a73c0",
+    "url": "/cryptohunter/static/js/2.ff98d451.chunk.js"
+  },
+  {
+    "revision": "8041207c6ef8ff1fb65254e32bc18cb7",
+    "url": "/cryptohunter/static/js/2.ff98d451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7497d8db9190c3fd3221",
+    "url": "/cryptohunter/static/js/main.28f05155.chunk.js"
+  },
+  {
+    "revision": "d2fe4b549936abd7f5e0",
+    "url": "/cryptohunter/static/js/runtime-main.63e9621c.js"
+  }
+]);
