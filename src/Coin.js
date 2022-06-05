10cbc76fc3d4786aadbc279b49663e907bb6a4d7
Eshaan Pandey <84571751+eshaan446@users.noreply.github.com>
2022-06-05 21:36:57 +0530

    Update Coin.js

--- src/Coin.js
+++ src/Coin.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-function Coin({name, icon, price, symbol, mktcap, pc1h, pc2h, pc3h, rank}) {
+function Coin({name, icon, price, symbol, mktcap, pc1h, pc2h, pc3h, rank, ts, as}) {
   return (
     <>
-        <div className='container coin'>
+        <div className='container coin text-center'>
             <h1>Name: {name} [{symbol}]</h1>
             <img src={icon}/><br/><br/>
-            <h3> Current Price: <span id="details">{"$"+price}</span></h3>
+            <h3>Price: <span id="details">{"$"+price}</span></h3>
             <h3>Price change(1h): <span id="details2">{pc1h+"%"}</span></h3>
             <h3>Price change(1d): <span id="details2">{pc2h+"%"}</span></h3>
             <h3>Price change(1w): <span id="details2">{pc3h+"%"}</span></h3>
+            <h3>Available Supply: <span id="details">{as}</span>&nbsp;<i class="fa-solid fa-coins"></i></h3>
+            <h3>Total Supply: <span id="details">{ts}</span>&nbsp;<i class="fa-solid fa-coins"></i></h3>
             <h3>Market Cap: <span id="details">{"$"+mktcap}</span></h3>
             <h3>Rank as per Market cap: <span id="details3">{rank}</span></h3>
         </div>
